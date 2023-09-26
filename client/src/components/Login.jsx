da4d64420b036a35e1a4902393eeae573e0be61a
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-09-26 22:23:12 +0100

    create validation file for login and function on login component

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,25 +1,42 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import validation from './LoginValidation'
 
 
 function Login() {
+    const [values, setValues] = useState({
+        email: '',
+        password: ''
+    })
+    const [errors, setErrors] = useState({})
+
+    const handelInput = (e) => {
+        setValues(prev => ({...prev, [e.target.name]: [e.target.value]}))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setErrors(validation(values));
+    }
   return (
     <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
         <div className='bg-white p-3 rounded w-25'>
             <h2>Log in</h2>
-            <form action=''>
+            <form action='' onSubmit={handleSubmit}>
                 
                 <div className='mb-3'>
                     <label htmlFor='email'><strong>Email</strong></label>
-                    <input type='email' placeholder='Enter your Email' className='form-control rounded-0' />
+                    <input type='email' placeholder='Enter your Email' className='form-control rounded-0' onChange={handelInput} name='email' />
+                    {errors.email && <span className='text-danger'>{ errors.email }</span>}
                 </div>
                 
                 <div className='mb-3'>
                     <label htmlFor='password'><strong>Password</strong></label>
-                    <input type='password' placeholder='Enter your Password' className='form-control rounded-0' />
+                    <input type='password' placeholder='Enter your Password' className='form-control rounded-0' onChange={handelInput} name='password' />
+                    {errors.password && <span className='text-danger'>{ errors.password }</span>}
                 </div>
                 
-                <button className='btn btn-success w-100 rounded-0'><strong>Log in</strong></button>
+                <button type='submit' className='btn btn-success w-100 rounded-0'><strong>Log in</strong></button>
                 <p className='mt-2'>You are agree to our terms and policies</p>
                 <Link to="/signup" className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Create Account</Link>
                 

b5f8ab058d6ed786d13777a268921e5b72d05e5a
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-09-26 21:24:15 +0100

    done with create sign up component

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 function Login() {
   return (
     <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
         <div className='bg-white p-3 rounded w-25'>
+            <h2>Log in</h2>
             <form action=''>
                 
                 <div className='mb-3'>
@@ -18,7 +21,7 @@ function Login() {
                 
                 <button className='btn btn-success w-100 rounded-0'><strong>Log in</strong></button>
                 <p className='mt-2'>You are agree to our terms and policies</p>
-                <button className='btn btn-default border w-100 bg-light rounded-0'>Create Account</button>
+                <Link to="/signup" className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Create Account</Link>
                 
             </form>
         </div>
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 function Signup() {
   return (
-    <div>Signup</div>
+     <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
+        <div className='bg-white p-3 rounded w-25'>
+            <h2>Sign-Up</h2>
+            <form action=''>
+                
+                <div className='mb-3'>
+                    <label htmlFor='Name'><strong>Name</strong></label>
+                    <input type='text' placeholder='Enter your Name' className='form-control rounded-0' />
+                </div>
+                
+                <div className='mb-3'>
+                    <label htmlFor='email'><strong>Email</strong></label>
+                    <input type='email' placeholder='Enter your Email' className='form-control rounded-0' />
+                </div>
+                
+                <div className='mb-3'>
+                    <label htmlFor='password'><strong>Password</strong></label>
+                    <input type='password' placeholder='Enter your Password' className='form-control rounded-0' />
+                </div>
+                
+                <button className='btn btn-success w-100 rounded-0'><strong>Sign up</strong></button>
+                <p className='mt-2'>You are agree to our terms and policies</p>
+                <Link to="/" className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Login</Link>
+                
+            </form>
+        </div>
+    </div>
   )
 }
 
